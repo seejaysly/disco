fd777fe0965ffe07647058a34399325ed327e02f
MehdiDev <46635765+MehdiMamas@users.noreply.github.com>
2022-01-04 22:25:27 +0100

    text updated

---
+++ Privacy-Policy.md
@@ -0,0 +1,33 @@
+Updated: Jan 1, 2020
+
+# Privacy Notice
+
+This privacy notice discloses the privacy practices for the Disco capture tool extension. This privacy notice applies solely to information collected by this extension. It will notify you:
+
+1. What personally identifiable information is collected from you through the website, how it is used, and with whom it may be shared.
+2. The security procedures in place to protect the misuse of your information.
+3. How you can correct any inaccuracies in the information.
+
+## Information Collection, Use, and Sharing
+
+We are the sole owners of the information collected from this extension. We only have access to log data. We will not sell or rent this information to anyone.
+
+### Log Data
+
+Like many site operators, we collect information that your browser sends whenever you visit our Site ("Log Data"). This Log Data may include information such as your computer's Internet Protocol ("IP") address, browser type, browser version, the pages of our Site that you visit, the time and date of your visit, the time spent on those pages and other statistics. We never collect the content of notes or information about the sites users visit. In addition, we may use third-party services such as Google Analytics that collect, monitor, and analyze this log data.
+
+### Google Analytics
+
+We use Google Analytics to collect information about use of this site. Google Analytics collects information such as how often users visit this site and what pages they visit when they do so. We also collect statistics about what actions users take. We use the information from Google Analytics to improve this extension only. Google Analytics collects only the IP address assigned to you on the date you visit this site and does not collect your name, email, or other identifying information. We do not combine the information collected through the use of Google Analytics with personally identifiable information.
+
+Although Google Analytics includes a cookie on your web browser that identifies you as a unique user the next time you visit this site, the cookie cannot be used by anyone but Google. Google’s ability to use and share information collected by Google Analytics about your visits to this site is restricted by the Google Analytics Terms of Use (as amended for government websites) and the Google Privacy Policy. You can prevent Google Analytics from recognizing you on return visits by disabling cookies on your browser.
+
+### Security
+
+We take precautions to protect your information. Sensitive information is only transmitted between the Chrome extension on your computer and with Google servers.
+While we use encryption to protect sensitive information transmitted online, within the Chrome Extension, information is stored in Javascript memory and local storage. While Chrome protects this information from being accessed by software other than Disco, this information is still potentially vulnerable.
+
+### Your Access to and Control Over Information
+
+If you feel we are not abiding by this privacy policy, please contact us immediately via the Chrome Webstore listings:
+https://chrome.google.com/webstore/detail/jfiabcklnnhkmkcdjjpmgghiimjkaeio
---
+++ js/options.js
@@ -0,0 +1,135 @@
+(function () {
+    var bgPage = chrome.extension.getBackgroundPage();
+
+    document.addEventListener("DOMContentLoaded", function () {
+        document
+            .querySelector("#revoke")
+            .addEventListener("click", revokeToken);
+        document.querySelector("#donate").addEventListener("click", donate);
+        document
+            .querySelector("#view_policy")
+            .addEventListener("click", policy);
+        document
+            .querySelector("#analytics")
+            .addEventListener("change", analytics);
+        document.getElementById("configure").addEventListener("click", (e) => {
+            chrome.tabs.create({
+                url: "chrome://extensions/configureCommands",
+            });
+        });
+        initUI();
+        initCommands();
+    });
+
+    function revokeToken() {
+        ga("send", "event", "Button", "Revoke Token");
+
+        // chrome.identity.getAuthToken(
+        //     {
+        //         interactive: true,
+        //     },
+        //     function (current_token) {
+        //         if (!chrome.runtime.lastError) {
+        //             // Remove the local cached token
+        //             chrome.identity.removeCachedAuthToken(
+        //                 {
+        //                     token: current_token,
+        //                 },
+        //                 function () {}
+        //             );
+
+        //             // Make a request to revoke token in the server
+        //             var xhr = new XMLHttpRequest();
+        //             xhr.open(
+        //                 "GET",
+        //                 "https://accounts.google.com/o/oauth2/revoke?token=" +
+        //                     current_token
+        //             );
+
+        //             xhr.onload = function (e) {
+        //                 console.log("onload", this, e.target);
+        //                 this.lastResponse = this.response;
+        //             }.bind(this);
+
+        //             xhr.onerror = function (e) {
+        //                 console.log(this, this.status, this.response);
+        //             };
+
+        //             xhr.send();
+
+        //             document.getElementById("revoke").disabled = true;
+        //             console.log(
+        //                 "Token revoked and removed from cache. " +
+        //                     "Check chrome://identity-internals to confirm."
+        //             );
+        //             initUI();
+        //         }
+        //     }
+        // );
+        bgPage.gdocs.revokeAuthToken(function () {
+            document.getElementById("revoke").disabled = true;
+            console.log(
+                "Token revoked and removed from cache. " +
+                    "Check chrome://identity-internals to confirm."
+            );
+            initUI();
+        });
+    }
+
+    function donate(e) {
+        ga("send", "event", "Button", "Donate", "Options");
+    }
+
+    function policy(e) {
+        ga("send", "event", "Button", "View Policy");
+    }
+
+    function initCommands() {
+        chrome.commands.getAll((all) => {
+            document.getElementById("browser_action").innerText = browserAction(
+                all
+            );
+        });
+    }
+
+    function browserAction(all) {
+        for (var i of all) {
+            if (i.name == "_execute_browser_action") {
+                return i.shortcut;
+            }
+        }
+        return null;
+    }
+
+    function initAnalytics(response) {
+        var input = document.querySelector("#analytics");
+        input.checked =
+            response.analytics == undefined ? true : response.analytics;
+        input.disabled = false;
+    }
+
+    function analytics(e) {
+        chrome.storage.sync.set({
+            analytics: e.target.checked,
+        });
+    }
+
+    function initUI() {
+        bgPage.toggleAuth(false, function (token) {
+            // Use the token.
+            if (!token) {
+                //if (!bgPage.oauth.hasToken()) {
+                document.getElementById("revoke").disabled = true;
+            } else {
+                console.log("Success!");
+                // access_token = token;
+                document.getElementById("revoke").disabled = false;
+            }
+            // if (chrome.runtime.lastError) {
+            //     console.log("Oops", chrome.runtime.lastError);
+            // }
+
+            chrome.storage.sync.get("analytics", initAnalytics);
+        });
+    }
+})();
